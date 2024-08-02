3db0ce89a8516a177f7f3e54440919def48eb7c0
Harrish Raj <99864981+Harrishraj27@users.noreply.github.com>
2024-08-03 00:18:35 +0100

    Create index.js

---
+++ frontend/store/index.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+  // Define your state properties here if needed
+});
+
+export const mutations = {
+  // Define your mutations here if needed
+};
+
+export const actions = {
+  // Define your actions here if needed
+};
