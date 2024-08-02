e875c607ef03deeb1620c2569907d9d5b122076a
Harrish Raj <99864981+Harrishraj27@users.noreply.github.com>
2024-08-03 00:23:33 +0100

    Create default.vue

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  // Add any necessary layout-level scripts here
+};
+</script>
+
+<style>
+/* Global styles can be added here */
+body {
+  font-family: Arial, sans-serif;
+}
+</style>
